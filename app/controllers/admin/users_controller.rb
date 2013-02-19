848d6c50501a470fd968920723e2a57bdbaf4ee8
Kris Luminar <kris.luminar@gmail.com>
2013-02-18 19:55:09 -0800

    Add the ablity to create users via admin backend

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,36 @@
 class Admin::UsersController < Admin::BaseController
+  before_filter :find_user, :only => [:show,
+                                      :edit,
+                                      :update,
+                                      :destroy]
+  def index
+    @users = User.all(order: "email")
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:notice] = "User has been created."
+      # redirect_to admin_user_path(@user) # why not this?
+      redirect_to admin_users_path
+    else
+      flash.now[:alert] = "User has not been created."
+      render action: 'new'
+    end
+  end
+
+  def show
+  end
+
+  private
+  def find_user
+    @user = User.find(params[:user])
+  rescue ActiveRecord::RecordNotFound
+    flash[:alert] = "The user you were looking for was not found."
+    redirect_to admin_root
+  end
 end
